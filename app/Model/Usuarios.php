e3ab527ba53b22389a4348f02bdfbf799fdca42c
rafaeldoria <ieieldoria@hotmail.com>
2018-04-29 01:49:03 -0300

    #11 Ajustes código.

--- app/Model/Usuarios.php
+++ app/Model/Usuarios.php
@@ -13,10 +13,10 @@ class Usuarios
     public function allUsuarios()
     {
         $i = 0;
-        $query = "select u.login, u.nome_usuario, c.desc_conta from Usuarios u
+        $query = "SELECT u.login, u.nome_usuario, c.desc_conta from Usuarios u
         INNER JOIN Contas c ON c.conta_id = u.conta_id";
         $result = mysqli_query($this->conn, $query);
-        if($result->num_rows > 0) {
+        if($result) {
             while($row = $result->fetch_assoc()) {
                 $ret[$i] = $row;
                 $i++;
@@ -29,7 +29,7 @@ class Usuarios
 
     public function getUsuario($id)
     {
-        $query = "select u.login, u.nome_usuario, c.desc_conta from Usuarios u
+        $query = "SELECT u.login, u.nome_usuario, c.desc_conta from Usuarios u
         INNER JOIN Contas c ON c.conta_id = u.conta_id
         where u.usuario_id = ".$id."";
         $result = mysqli_query($this->conn, $query);
@@ -38,35 +38,51 @@ class Usuarios
 
     public function addUsuario($data)
     {
-        $sql = "INSERT INTO Usuarios (login, senha, nome_usuario, conta_id, dt_created, dt_updated)
+        $query = "INSERT INTO Usuarios (login, senha, nome_usuario, conta_id, dt_created, dt_updated)
                 VALUES ('".$data["login"]."', '".$data["senha"]."', '".$data["nome_usuario"]."', '".$data["conta_id"]."', '".date('Y-m-d H:i:s')."', '".date('Y-m-d H:i:s')."')";
 
-        if ($this->conn->query($sql)) {
+        if ($this->conn->query($query)) {
             return "Novo usuário adicionado com sucesso";
         } else {
-            return "Error: " . $sql . "<br>" . $this->conn->error;
+            return "Error: " . $query . "<br>" . $this->conn->error;
         }
     }
 
     public function updateUsuario($data)
     {
-        $sql = "UPDATE Usuarios SET login='".$data["login"]."', nome_usuario='".$data["nome_usuario"]."', conta_id='".$data["conta_id"]."', dt_updated='".date('Y-m-d H:i:s')."'
+        $query = "UPDATE Usuarios SET login='".$data["login"]."', nome_usuario='".$data["nome_usuario"]."', conta_id='".$data["conta_id"]."', dt_updated='".date('Y-m-d H:i:s')."'
         WHERE usuario_id=".$data["usuario_id"]."";
-        if ($this->conn->query($sql)) {
+        if ($this->conn->query($query)) {
             return "Usuario atualizado com sucesso";
         } else {
-            return "Error: " . $sql . "<br>" . $this->conn->error;
+            return "Error: " . $query . "<br>" . $this->conn->error;
         }
     }
 
     public function deleteUsuario($id)
     {
-        $sql = "DELETE from Usuarios where conta_id = ".$id."";
+        $query = "DELETE from Usuarios where conta_id = ".$id."";
 
-        if ($this->conn->query($sql)) {
+        if ($this->conn->query($query)) {
             return "Usuario deletado";
         } else {
-            return "Error: " . $sql . "<br>" . $this->conn->error;
+            return "Error: " . $query . "<br>" . $this->conn->error;
+        }
+    }
+
+    public function auth($data)
+    {
+        $query = "SELECT * from Usuarios where login = '".$data["login"]."'";
+        $result = mysqli_query($this->conn, $query);
+        if($result) {
+            $user = $result->fetch_assoc();
+            if(base64_decode($user["senha"])==$data["senha"]) {
+                return $user;
+            }else {
+                return "Usuário ou senha incorretos.";
+            }
+        } else{
+            return "Usuário ou senha incorretos.";
         }
     }
 }
